50c9f0f7c3d1af19137841a5dce684e9870d2076
Lộc Nguyễn <banngloc@gmail.com>
2023-08-19 14:28:09 +0700

    Sửa được bug không lưu thư vào cơ sở dữ liệu

--- letters/send_letter.php
+++ letters/send_letter.php
@@ -16,19 +16,19 @@ if ($_SESSION["role"] !== 'user') {
     exit();
 }
 
+$servername = "localhost";
+$username = "emo";
+$password = "123456EmoR2";
+$dbname = "emo";
+
 // Kiểm tra xem có dữ liệu được gửi từ form soạn thư hay không
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    if (isset($_POST["recipient_name"]) && isset($_POST["title"]) && isset($_POST["content"])) {
-        $recipient_id = $_POST["recipient_name"];
+   if (isset($_POST["recipient_name"]) && isset($_POST["title"]) && isset($_POST["content"])) {
+        $recipient_name = $_POST["recipient_name"];
         $title = $_POST["title"];
         $content = $_POST["content"];
 
-        // Kết nối đến cơ sở dữ liệu (chú ý thay đổi thông tin kết nối phù hợp với máy bạn)
-        $servername = "localhost";
-        $username = "emo";
-        $password = "123456EmoR2";
-        $dbname = "emo";
-
+        // Kết nối đến cơ sở dữ liệu
         $conn = new mysqli($servername, $username, $password, $dbname);
 
         // Kiểm tra kết nối
@@ -36,24 +36,37 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             die("Kết nối thất bại: " . $conn->connect_error);
         }
 
-        // Thêm thông tin thư vào bảng "letters"
-        $sql_insert_email = "INSERT INTO letters (sender_id, receiver_id, title, content) VALUES (?, ?, ?, ?)";
-        $stmt_insert_email = $conn->prepare($sql_insert_email);
-        $stmt_insert_email->bind_param("iiss", $_SESSION["user_id"], $recipient_id, $title, $content);
-
-        if ($stmt_insert_email->execute()) {
-            $msg = "Thư đã được gửi thành công";
-            $_SESSION['msg'] = $msg;
-            header("Location: ../users/home.php");
+        $sql_get_id = "SELECT id FROM users WHERE username = ?";
+        $stmt_get_id = $conn->prepare($sql_get_id);
+        $stmt_get_id->bind_param("s", $recipient_name);
+        $stmt_get_id->execute();
+        $result = $stmt_get_id->get_result();
+        // Nếu có id phù hợp với tên nhận, thực hiện insert
+        if ($result->num_rows > 0) {
+            // Lấy ra id của tên nhận
+            $row = $result->fetch_assoc();
+            $recipient_id = $row['id'];
+            $sql_insert_email = "INSERT INTO letters (sender_id, receiver_id, title, content) VALUES (?, ?, ?, ?)";
+            $stmt_insert_email = $conn->prepare($sql_insert_email);
+            $stmt_insert_email->bind_param("iiss", $_SESSION["user_id"], $recipient_id, $title, $content);
+            // Thử gửi mail, nếu không được thì thông báo lại cho người dùng
+            if ($stmt_insert_email->execute()) {
+                $msg = "Thư đã được gửi thành công";
+                $_SESSION['msg'] = $msg;
+                header("Location: ../users/home.php");
+            } else {
+                $msg = "Đã xảy ra lỗi. Vui lòng thử lại!";
+                $_SESSION['msg'] = $msg;
+                header("Location: write_letter.php ");
+            }
+            // Đóng câu lệnh PreparedStatement
+            $stmt_insert_email->close();
         } else {
-            $msg = "Đã xảy ra lỗi. Vui lòng thử lại!";
+            $msg = "Tên người nhận không tồn tại!";
             $_SESSION['msg'] = $msg;
             header("Location: write_letter.php ");
+            exit();
         }
-
-        // Đóng câu lệnh Prepared Statement
-        $stmt_insert_email->close();
-
         // Đóng kết nối
         $conn->close();
     } else {
